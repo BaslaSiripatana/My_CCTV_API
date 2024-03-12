8d76a5199a67062e685d1b9ebfc8b451a4b98264
BaslaSiripatana <79196014+BaslaSiripatana@users.noreply.github.com>
2024-03-12 12:07:15 +0700

    test timeout setting

--- main.py
+++ main.py
@@ -67,7 +67,11 @@ async def predict(file: UploadFile = File(...)):
 #     uvicorn.run(app)
 
 
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
+
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
+    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)), timeout_keep_alive=120, timeout_notify=120)
+
 
 

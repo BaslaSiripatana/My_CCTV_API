16930d613c38edf3a567ce71daa3c71441a6cdab
BaslaSiripatana <79196014+BaslaSiripatana@users.noreply.github.com>
2024-03-12 11:11:25 +0700

    import os1

--- main.py
+++ main.py
@@ -66,6 +66,7 @@ async def predict(file: UploadFile = File(...)):
 # if __name__ == "__main__":
 #     uvicorn.run(app)
 
+
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
 
